e891a12c798eeeae6a219016a0640f38316ebe5c
Igor Santos <igr.exe@gmail.com>
2019-02-12 21:58:02 -0200

    fix(settings): change assert

--- settings.py
+++ settings.py
@@ -6,22 +6,22 @@ from tapioca_zendesk import Zendesk
 # Zendesk settings
 ZENDESK_API_TOKEN = os.getenv('ZENDESK_API_TOKEN')
 
-assert ZENDESK_API_TOKEN is None, 'ZENDESK_API_TOKEN not environment.'
+assert ZENDESK_API_TOKEN is not None, 'ZENDESK_API_TOKEN not environment.'
 
 ZENDESK_API_USER = os.getenv('ZENDESK_API_USER')
 
-assert ZENDESK_API_USER is None, 'ZENDESK_API_USER not environment.'
+assert ZENDESK_API_USER is not None, 'ZENDESK_API_USER not environment.'
 
 zendesk = Zendesk(user=ZENDESK_API_USER, password=ZENDESK_API_TOKEN)
 
 # Secret JWT settings
 JWT_SECRET = os.getenv('JWT_SECRET')
 
-assert JWT_SECRET is None, 'JWT_SECRET not environment.'
+assert JWT_SECRET is not None, 'JWT_SECRET not environment.'
 
 # Google Maps settings
 GOOGLE_MAPS_API_KEY = os.getenv('GOOGLE_MAPS_API_KEY')
 
-assert GOOGLE_MAPS_API_KEY is None, 'GOOGLE_MAPS_API_KEY not environment.'
+assert GOOGLE_MAPS_API_KEY is not None, 'GOOGLE_MAPS_API_KEY not environment.'
 
 gmaps = googlemaps.Client(key=GOOGLE_MAPS_API_KEY)
