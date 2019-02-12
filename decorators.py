4b0d9a1578767f354ebba849cf30c1d8e21b5d9f
Igor Santos <igr.exe@gmail.com>
2019-02-11 23:15:18 -0200

    chore(test): test stdin on staging

--- decorators.py
+++ decorators.py
@@ -10,13 +10,27 @@ def decode_jwt(serializer_class):
     def wrapper(func):
         @functools.wraps(func)
         def decode(*args, **kwargs):
-            token_input = sys.argv[1]
-            json = jwt.decode(
-                token_input,
-                settings.JWT_SECRET,
-                algorithms=['HS512'])
-            obj = serializer_class(**json)
-            log.info('Decoded JWT')
-            return func(obj, *args, **kwargs)
+            log.info('stdin')
+            log.info(sys.stdin.readline())
+            token_input = None
+            try:
+                token_input = sys.argv[1]
+            except IndexError:
+                log.info(
+                    'JWT input wasn\'t received like args. ' +
+                    'Reading stdin to search token input'
+                )
+                token_input = sys.stdin.readline()
+
+            if not token_input:
+                log.error('JWT input not received when command was called.')
+            else:
+                json = jwt.decode(
+                    token_input,
+                    settings.JWT_SECRET,
+                    algorithms=['HS512'])
+                obj = serializer_class(**json)
+                log.info('Decoded JWT')
+                return func(obj, *args, **kwargs)
         return decode
     return wrapper
