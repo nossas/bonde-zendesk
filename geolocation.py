31d57777cf32cb8ed620b2fff3d7d9bd6074ae8f
Igor Santos <igr.exe@gmail.com>
2019-02-21 00:27:17 -0300

    chore(geo): add exception when address not found.

--- geolocation.py
+++ geolocation.py
@@ -2,8 +2,16 @@ from settings import gmaps
 from serializers import GeocodeSchema
 
 
+class AddressNotFound(Exception):
+    pass
+
+
 def get_geocode(address, many=False):
     geocode_data = gmaps.geocode(address)
+
+    if len(geocode_data) == 0:
+        raise AddressNotFound('{0} not found.'.format(address))
+
     serializer = GeocodeSchema().load(
         geocode_data if many else geocode_data[0],
         many=many
