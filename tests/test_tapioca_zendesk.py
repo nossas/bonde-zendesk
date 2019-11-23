ea775cb661e5515d3e603df3e7d8217ececf47d6
Lucas Pirola <lucaspirola@gmail.com>
2019-11-23 00:30:43 -0300

    chore: add test to assert TapiocaClient

--- tests/test_tapioca_zendesk.py
+++ tests/test_tapioca_zendesk.py
@@ -1,15 +1,18 @@
 # coding: utf-8
 
 import unittest
+import os
 
 from tapioca_zendesk import Zendesk
-
+from tapioca.tapioca import TapiocaClient
 
 class TestTapiocaZendesk(unittest.TestCase):
 
     def setUp(self):
         self.wrapper = Zendesk()
 
+    def test_zendesk_wrapper(self):
+        self.assertIsInstance(self.wrapper, TapiocaClient)
 
 if __name__ == '__main__':
     unittest.main()
