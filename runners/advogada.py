498930c1c11eb80bcfa3dd3927d3d91fd83304bf
Viviane <vivianedias@protonmail.com>
2020-01-30 16:54:12 -0300

    fix: bugs after change fields from form entries

--- runners/advogada.py
+++ runners/advogada.py
@@ -9,11 +9,11 @@ class Runner(PsicologaRunner):
         'email': 'field-1533735752669-39',
         'phone': 'field-1533735813563-2',
         # Custom fields on Zendesk
-        'state': 'field-1533735770558-86',
-        'city': 'field-1533735788159-99',
+        # 'state': 'field-1533735770558-86',
+        # 'city': 'field-1533735788159-99',
         'address': 'field-1533735803691-45',
         'registration_number': 'field-1533735761357-93',
         'whatsapp': 'field-1533735832329-53',
-        'occupation_area': 'field-1533735911227-68',
+        # 'occupation_area': 'field-1533735911227-68',
         'disponibilidade_de_atendimentos': 'field-1533735888966-20',
     }
